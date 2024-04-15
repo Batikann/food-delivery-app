e2a0a8d90aa2ed6d54b3f461d4694b7874363f8f
Batikann <emiruar123@gmail.com>
2024-04-15 20:13:59 +0300

    first commit

---
+++ constants/index.ts
@@ -0,0 +1,22 @@
+export const headerLinks = [
+  {
+    label: 'Home',
+    route: '/',
+  },
+  {
+    label: 'Explore',
+    route: '/events/create',
+  },
+  {
+    label: 'Projects',
+    route: '/profile',
+  },
+  {
+    label: 'About Us',
+    route: '/profile',
+  },
+  {
+    label: 'Contact Us',
+    route: '/profile',
+  },
+]
